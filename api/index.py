506fc40969a52f9a441018856f17e408d862af11
Pranav Rayban <raybanpranav.mahesh2023@vitstudent.ac.in>
2025-03-05 17:55:19 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -149,3 +149,9 @@ def get_complaints():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+from vercel_wsgi import handle_wsgi
+
+def handler(event, context):
+    return handle_wsgi(app, event, context)
+
